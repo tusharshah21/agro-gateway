96d6d7332313b3af075cf1bd5b06311ef0af4794
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-10-17 23:25:10 +0300

    Added pages to AO

--- routes/ao_routes.js
+++ routes/ao_routes.js
@@ -2,6 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 // register route
+router.get("/", (req, res) => {
+	res.render("ao/ao_dash.pug");
+});
+
+router.get("/members", (req, res) => {
+	res.render("ao/members");
+});
+
+router.get("/gp", (req, res) => {
+	res.render("ao/general_public");
+});
+
 router.get("/register", (req, res) => {
 	res.render("ao/registration");
 });
@@ -9,6 +21,10 @@ router.get("/register", (req, res) => {
 router.post("/register", (req, res) => {
 	console.log(req.body);
 	res.send(`<h1 style='text-align:center;margin-top:10%;font-size:70px'>Registered 😊</h1>`);
+});
+
+router.get("/products", (req, res) => {
+	res.render("products");
 });
 
 module.exports = router;
